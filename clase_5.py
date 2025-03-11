894534e62c2412381a5b3d7819790eddfa01b3a4
Mariel <marielvertz@gmail.com>
2025-03-10 22:16:15 -0600

    Add files via upload

---
+++ clase_5.py
@@ -0,0 +1,5 @@
+import numpy as np
+survey_responses=np.array (["BUEENO"," EXCELENTE", "MALO",
+                            "BUENO","MALO", "EXCELENTE",
+                            "BUENO", "BUENO", "MALO",
+                            "ECELENTE"])
