554ba504ca7eb366e8730b959520e66de4500741
Mariel <marielvertz@gmail.com>
2025-03-25 20:41:51 -0600

    Add files via upload

---
+++ iteradores_cadenas.py
@@ -0,0 +1,12 @@
+#ITERAR EN CADENA
+#CREANDO LA CADENA 
+text="HOLA MUNDO "
+
+
+#CREANDO EL ITERADOR
+iter_text=iter(text) #se llama a la funcion iter 
+
+
+#ITERAR EN LA CADENA 
+for char in iter_text: #se itera usando el bucle fot 
+    print(char)
